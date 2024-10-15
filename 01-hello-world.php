673d333c87be23ecd5f1597b0f9942541a723572
luaXoXo <79286488+luaXoXo@users.noreply.github.com>
2024-10-15 14:44:35 -0300

    helloworld

---
+++ 01-hello-world.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciando com php</title>
+</head>
+<body>
+
+    <h1>Experiencias com PHP</h1>
+    <hr>
+    
+    <?php
+
+    ?>
+
+</body>
+</html>
