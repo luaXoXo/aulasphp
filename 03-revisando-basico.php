e964a0377fe6f5ebc38f23a72832f8d98909bb4e
luaXoXo <79286488+luaXoXo@users.noreply.github.com>
2024-10-16 16:43:38 -0300

    arrays

--- 03-revisando-basico.php
+++ 03-revisando-basico.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
 </head>
+
 <body>
 
     <h1>Revisando o basico</h1>
@@ -20,14 +22,15 @@
     $idade = 25;
     /* exibir no HTML duas frase da seguinte forma */
 
-    echo "<p>Ola! Meu nome é <i>".NOME."<i> e eu tenho <span class='text-bg-danger'> $idade anos</span>"
-    
-    
+    echo "<p>Ola! Meu nome é <i>" . NOME . "<i> e eu tenho <span class='text-bg-danger'> $idade anos</span>"
+
+
     ?>
-    <p>Olá! meu nome é <i class="text-danger"><?=NOME?></i> e tenho <span class=text-bg-danger ><?=$idade?> anos.</span></p>
+    <p>Olá! meu nome é <i class="text-danger"><?= NOME ?></i> e tenho <span class=text-bg-danger><?= $idade ?> anos.</span></p>
 
-    <p >Qualquer coisa </p>
+    <p>Qualquer coisa </p>
 
-    
+
 </body>
+
 </html>
--- 04-arrays.php
+++ 04-arrays.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,17 +8,42 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
     <h1>Arrays no PHP</h1>
     <hr>
     <?php
     $bandas = ["Slayer", "Nightwish", "Pink Floyd", "Savatage"];
-    $cursos = array ("node.js", "desing", "nutrição")
-?>
+    $cursos = array("node.js", "desing", "nutrição")
+    ?>
 
-<h2>Acessando os dados do array</h2>
-<p>Rock Progressivo: <?=$bandas[2] ?></p>
-<p>Otem estivemos com a turma de <?=$cursos[2] ?></p>
+    <h2>Acessando os dados do array</h2>
+    <p>Rock Progressivo: <?= $bandas[2] ?></p>
+    <p>Otem estivemos com a turma de <?= $cursos[2] ?></p>
+
+
+
+
+        <?php 
+        
+        
+        $filme = [
+            "titulo" => "star wars",
+            "autor" => "george lucas",
+            "lançamento" => "slakkkkk",
+            "genero" => "ficção cientifica"
+        ];
+        ?>
+        <h2>Acessando os dados do array associativo</h2>
+        <p>O filme <?=$filme["titulo"]?> feito pelo mano <?=$filme["autor"] ?> em <?=$filme["lançamento"]?> qeu ano do genero <?=$filme["genero"] ?> </p>
+
+        <h3>print_r(nomeArray)</h3>
+        <pre><?=print_r($cursos) ?></pre>
+
+        <h3>var_dump(nomeArray)</h3>
+       <pre> <?=var_dump($cursos) ?></pre>
+
 
 </body>
+
 </html>
